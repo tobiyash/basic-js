e4698cb98ee335fc638e8f3c65935a8409b10638
tobiyash <70479970+tobiyash@users.noreply.github.com>
2023-04-17 00:22:43 +0300

    feat: solve repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let res = [];
+  let addRes = [];
+  let times = options.hasOwnProperty("repeatTimes") ? options.repeatTimes : 1;
+  let addTimes = options.hasOwnProperty("additionRepeatTimes") ? options.additionRepeatTimes : 1;
+  let sep = options.hasOwnProperty("separator") ? options.separator + "" : "+";
+  let add = options.hasOwnProperty("addition") ? options.addition + "" : "";
+  let addSep = options.hasOwnProperty("additionSeparator") ? options.additionSeparator : "|";
+  for (let i = 0; i < addTimes; i++) {
+    addRes.push(add);
+  }
+  addRes = addRes.join(addSep);
+  for (let i = 0; i < times; i++) {
+    res.push(str+addRes);
+  }
+  return res.join(sep);
 }
 
 module.exports = {
