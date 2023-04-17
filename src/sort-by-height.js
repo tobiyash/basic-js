5545d67f67be5e9fab4a45dd147877d74cbcc5de
tobiyash <70479970+tobiyash@users.noreply.github.com>
2023-04-17 15:00:13 +0300

    feat: sort-by-height

--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -11,9 +11,16 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function sortByHeight(arr) {
+  const newArr = arr.reduce((acc, el) => {
+    if (el === -1) return acc;
+    acc.push(el);
+    return acc;
+  }, []).sort((a, b) => a - b);
+  arr.forEach((el, i) => {
+    if (el === -1) newArr.splice(i, 0, -1);
+  })
+  return newArr;
 }
 
 module.exports = {
